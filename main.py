eeff8362cd114e132535d66a2dbd0a98c7c95188
nimmy Joy <nimmyjoykarickadu@gmail.com>
2023-01-17 22:55:35 +0530

    updated data ingestion part

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ all_record = collection.find()
 for idx, record in enumerate(all_record):
      print(f"{idx}: {record}")
 
-     """
+   
 from sensor.logger import logging
 from sensor.exception import SensorException
 import sys,os
@@ -41,5 +41,25 @@ def test_logger_and_exception():
 if __name__=="__main__":
      try:
           test_logger_and_exception()
+     except Exception as e:
+          print(e)
+
+            """
+from sensor.logger import logging
+from sensor.exception import SensorException
+from sensor.utils import get_collection_as_dataframe
+import sys,os
+from sensor.entity import config_entity
+from sensor.components.data_ingestion import DataIngestion
+
+
+
+if __name__=="__main__":
+     try:
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
+          data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+        
+          data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion())
      except Exception as e:
           print(e)
